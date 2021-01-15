f880e55af837c67491e373ba3a5bc7832f2088b6
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-01-14 23:48:50 -0700

    turns out what I'm doing is called literate programming

---
+++ util.py
@@ -0,0 +1,12 @@
+import numpy as np
+import numpy_financial as npf
+import sympy as sym
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# simple interest
+def fv_simple(P, i, N):
+    return P * (1 + i*N)
+# compound interest
+def fv_compound(P, i, N):
+    return P * (1 + i)**N
