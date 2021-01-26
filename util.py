e8440786d8f235a07ae3d5eeae1609e875d9309a
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-01-26 01:44:04 -0700

    I wanna kms

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import numpy_financial as npf
 import sympy as sym
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 
 class FormulaError(Exception):
@@ -19,165 +20,187 @@ class FormulaError(Exception):
 
     def __str__(self):
         return f"{self.factor_notation} -> {self.message}"
-        
 
-def simple_interest(**kwargs):
+
+def simple_interest(P=None, i=None, N=None):
     fn = "simple interest"
-    if 'P' not in kwargs:
+    if P is None:
         raise FormulaError(fn, "P, or principle amount not provided")
-    elif 'i' not in kwargs:
-        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-    elif 'N' not in kwargs:
+    elif i is None or i <=0 or 1 <= i:
+        raise FormulaError(fn, "i, or interest not provided: 0<i<1")
+    elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    P = kwargs['P']
-    i = kwargs['i']
-    N = kwargs['N']
     return P * (1 + i*N)
 
-def single_compound(**kwargs):
-    fn = "single cash flow (F/P, i, N)" 
-    if 'P' not in kwargs:
+def single_future(P=None, i=None, N=None):
+    fn = "single cash flow (F/P, i, N)"
+    if P is None:
         raise FormulaError(fn, "P, or principle amount not provided")
-    elif 'i' not in kwargs:
+    elif i is None:
         raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-    elif 'N' not in kwargs:
+    elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    P = kwargs['P']
-    i = kwargs['i']
-    N = kwargs['N']
     return P * (1 + i)**N
 
-def single_present(**kwargs):
+def single_present(F=None, i=None, N=None):
     fn = "single cash flow (P/F, i, N)"
-    if 'F' not in kwargs:
+    if F is None:
         raise FormulaError(fn, "F, or future amount not provided")
-    elif 'i' not in kwargs:
+    elif i is None:
         raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-    elif 'N' not in kwargs:
+    elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    F = kwargs['F']
-    i = kwargs['i']
-    N = kwargs['N']
     return F/(1 + i)**N
 
-def eq_compound(**kwargs):
-    fn = "equal payment series (F/A, i, N)"
-    if 'A' not in kwargs:
-        raise FormulaError(fn, "A, or future amount not provided")
-    elif 'i' not in kwargs:
-        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-    elif 'N' not in kwargs:
-        raise FormulaError(fn, "N, or number of number of interest periods is missing")
-
-    A = kwargs['A']
-    i = kwargs['i']
-    N = kwargs['N']
-    return A * ((1+i)**N -1) / i 
-        
-def eq_sinking(**kwargs):
-    fn = "equal payment series (A/F, i N)"
-    if 'F' not in kwargs:
+def single_rate(F=None, P=None, N=None):
+    fn = "find the discounting rate that makes a present amount equivalent to a future amount"
+    if F is None:
         raise FormulaError(fn, "F, or future amount not provided")
-    elif 'i' not in kwargs:
-        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-    elif 'N' not in kwargs:
+    if P is None:
+        raise FormulaError(fn, "P, or present amount not provided")
+    elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    F = kwargs['F']
-    i = kwargs['i']
-    N = kwargs['N']
+    return npf.rate(N, 0, -P, F)
+
+
+def single_periods(F=None, P=None, i=None):
+    fn = "find the number of periods makes a present amount equivalent to a future amount"
+    if F is None:
+        raise FormulaError(fn, "F, or future amount not provided")
+    if P is None:
+        raise FormulaError(fn, "P, or present amount not provided")
+    elif i is None:
+        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
+
+    return npf.nper(i, 0, -P, F)
+
+def eq_future(A=None, i=None, N=None):
+    fn = "equal payment series (F/A, i, N)"
+    if A is None:
+        raise FormulaError(fn, "A, or future amount not provided")
+    elif i is None:
+        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
+    elif N is None:
+        raise FormulaError(fn, "N, or number of number of interest periods is missing")
+
+    return A * ((1+i)**N -1) / i
+
+def eq_sinking(F=None, i=None, N=None):
+    fn = "equal payment series (A/F, i N): gives future value"
+    if F is None:
+        raise FormulaError(fn, "F, or future amount not provided")
+    elif i is None:
+        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
+    elif N is None:
+        raise FormulaError(fn, "N, or number of number of interest periods is missing")
+
     return F * i/ ((1+i)**N - 1)
 
-def eq_present(**kwargs):
+def eq_present(A=None, i=None, N=None):
     fn = "equal payment (P/A, i, N)"
-    if 'A' not in kwargs:
+    if A is None:
         raise FormulaError(fn, "A, or future amount not provided")
-    elif 'i' not in kwargs:
+    elif i is None:
         raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-    elif 'N' not in kwargs:
+    elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
-
-    A = kwargs['A']
-    i = kwargs['i']
-    N = kwargs['N']
 
     return A * ((1+i)**N - 1) / (i * (1+i)**N)
 
-def eq_captial_recovery(**kwargs):
-    fn = "equal payment (A/P, i, N)"
-    if 'P' not in kwargs:
+def eq_capital_recovery(P=None, i=None, N=None):
+    fn = "equal payment (A/P, i, N): gives A"
+    if P is None:
         raise FormulaError(fn, "P, or principle amount not provided")
-    elif 'i' not in kwargs:
+    elif i is None:
         raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-    elif 'N' not in kwargs:
+    elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    P = kwargs['P']
-    i = kwargs['i']
-    N = kwargs['N']
-
     return P * (i * (1 + i)**N) / ((1 + i)**N - 1)
-    
 
-def linear_present(**kwargs):
+
+def linear_present(G=None, i=None, N=None):
    fn = "gradient series (P/G, i, N)"
-   if 'G' not in kwargs:
+   if G is None:
        raise FormulaError(fn, "G, or the first payment at period 2 is missing")
-   elif 'i' not in kwargs:
+   elif i is None:
        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-   elif 'N' not in kwargs:
+   elif N is None:
        raise FormulaError(fn, "N, or number of number of interest periods is missing")
-
-   G = kwargs['G']
-   i = kwargs['i']
-   N = kwargs['N']
 
    return G * ( (1+i)**N - i*N - 1 ) / (i**2 * (1 + i)**N)
 
 
-def linear_conversion(**kwargs):
-   fn = "gradient series (A/G, i, N)"
-   if 'G' not in kwargs:
+def linear_conversion(G=None, i=None, N=None):
+   fn = "gradient series (A/G, i, N): gives A"
+   if G is None:
        raise FormulaError(fn, "G, or the first payment at period 2 is missing")
-   elif 'i' not in kwargs:
+   elif i is None:
        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-   elif 'N' not in kwargs:
+   elif N is None:
        raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-   G = kwargs['G']
-   i = kwargs['i']
-   N = kwargs['N']
-
    return G * ( (1+i)**N - i*N - 1) / (i * ((1+i)**N - 1))
-    
-    
-def geo_present(**kwargs):
-    fn = "geometric gradient series (P/A_1, g, i, N)"
-    if 'A1' not in kwargs:
-        raise FormulaError(fn, "A1, or the first payment at period 1 is missing")
-    elif 'g' not in kwargs:
-        raise FormulaError(fn, "g, or the growth rate is missing")
-    elif 'i' not in kwargs:
-        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-    elif 'N' not in kwargs:
-        raise FormulaError(fn, "N, or number of number of interest periods is missing")
-
-    A1 = kwargs['A1']
-    g = kwargs['g']
-    i = kwargs['i']
-    N = kwargs['N']
-
-    if i == g:
-        return A1 * N / (1+i)
-
-    return A1 * (1 - (1+g)**N * (1+i)**(-N)) / (i-g)
-
-        
-def effective_rate_per_interest_period(r, M, K):
-    return (1+ r/M)**(M/K) -1
 
 
-print(eq_sinking(F=20000, i=0.09, N=5))
+def geo_present(A1=None, g=None, i=None, N=None):
+   fn = "geometric gradient series (P/A_1, g, i, N)"
+   if A1 is None:
+       raise FormulaError(fn, "A1, or the first payment at period 1 is missing")
+   elif g is None:
+       raise FormulaError(fn, "g, or the growth rate is missing")
+   elif i is None:
+       raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
+   elif N is None:
+       raise FormulaError(fn, "N, or number of number of interest periods is missing")
+
+   if i == g:
+       return A1 * N / (1+i)
+
+   return A1 * (1 - (1+g)**N * (1+i)**(-N)) / (i-g)
+
+
+def effective_rate_per_interest_period(r=None, M=None, K=None, C=None):
+    fn = """effective_rate_per_interest_period: i_a
+    r = APR or nominal interest rate per year
+    M = number of compounding periods per year, M=CK
+    C = number of compounding periods per payment period
+    K = number of payment periods per year
+    """
+
+    if r is None:
+        raise FormulaError(fn, "r, or APR, or nominal interest rate per year is missing")
+
+    if M == math.inf and K is not None:
+        return math.exp(r/K) -1
+
+    if M == math.inf and K is None:
+        return math.exp(r/K) -1
+
+    if C is not None and K is not None and M is None:
+        return (1+ r/(C*K))**C - 1
+
+    if K is None and C is None and M is not None:
+        return (1 + r/M)**M - 1
+
+    if C is not None and K is None and M is not None:
+        return (1+r/M)**C - 1
+
+    if M is not None and K is not None and C is None and M is not None:
+        return (1+ r/M)**(M/K) -1
+
+    raise FormulaError(fn, "invalid combo of inputs")
+
+# amortized
+def remaining_balance():
+    pass
+
+def interest_payment():
+    pass
+
+def principal_repayment():
+    pass
