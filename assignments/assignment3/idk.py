add83a43c6ea8df87ba6976ae729313d216b0652
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-02-02 01:18:41 -0700

    I need sleep

---
+++ assignments/assignment3/idk.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from util import *
+
+x = YTM(1000, 0.12, 30, 2, 1000)
+print(x)
--- util.py
+++ util.py
@@ -26,12 +26,13 @@ def simple_interest(P=None, i=None, N=None):
     fn = "simple interest"
     if P is None:
         raise FormulaError(fn, "P, or principle amount not provided")
-    elif i is None or i <=0 or 1 <= i:
+    elif i is None or i <= 0 or 1 <= i:
         raise FormulaError(fn, "i, or interest not provided: 0<i<1")
     elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    return P * (1 + i*N)
+    return P * (1 + i * N)
+
 
 def single_future(P=None, i=None, N=None):
     fn = "single cash flow (F/P, i, N)"
@@ -42,7 +43,8 @@ def single_future(P=None, i=None, N=None):
     elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    return P * (1 + i)**N
+    return P * (1 + i) ** N
+
 
 def single_present(F=None, i=None, N=None):
     fn = "single cash flow (P/F, i, N)"
@@ -53,7 +55,8 @@ def single_present(F=None, i=None, N=None):
     elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    return F/(1 + i)**N
+    return F / (1 + i) ** N
+
 
 def single_rate(F=None, P=None, N=None):
     fn = "find the discounting rate that makes a present amount equivalent to a future amount"
@@ -78,6 +81,7 @@ def single_periods(F=None, P=None, i=None):
 
     return npf.nper(i, 0, -P, F)
 
+
 def eq_future(A=None, i=None, N=None):
     fn = "equal payment series (F/A, i, N)"
     if A is None:
@@ -87,7 +91,8 @@ def eq_future(A=None, i=None, N=None):
     elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    return A * ((1+i)**N -1) / i
+    return A * ((1 + i) ** N - 1) / i
+
 
 def eq_sinking(F=None, i=None, N=None):
     fn = "equal payment series (A/F, i N): gives future value"
@@ -98,7 +103,8 @@ def eq_sinking(F=None, i=None, N=None):
     elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    return F * i/ ((1+i)**N - 1)
+    return F * i / ((1 + i) ** N - 1)
+
 
 def eq_present(A=None, i=None, N=None):
     fn = "equal payment (P/A, i, N)"
@@ -109,7 +115,8 @@ def eq_present(A=None, i=None, N=None):
     elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    return A * ((1+i)**N - 1) / (i * (1+i)**N)
+    return A * ((1 + i) ** N - 1) / (i * (1 + i) ** N)
+
 
 def eq_capital_recovery(P=None, i=None, N=None):
     fn = "equal payment (A/P, i, N): gives A"
@@ -120,48 +127,48 @@ def eq_capital_recovery(P=None, i=None, N=None):
     elif N is None:
         raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-    return P * (i * (1 + i)**N) / ((1 + i)**N - 1)
+    return P * (i * (1 + i) ** N) / ((1 + i) ** N - 1)
 
 
 def linear_present(G=None, i=None, N=None):
-   fn = "gradient series (P/G, i, N)"
-   if G is None:
-       raise FormulaError(fn, "G, or the first payment at period 2 is missing")
-   elif i is None:
-       raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-   elif N is None:
-       raise FormulaError(fn, "N, or number of number of interest periods is missing")
+    fn = "gradient series (P/G, i, N)"
+    if G is None:
+        raise FormulaError(fn, "G, or the first payment at period 2 is missing")
+    elif i is None:
+        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
+    elif N is None:
+        raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-   return G * ( (1+i)**N - i*N - 1 ) / (i**2 * (1 + i)**N)
+    return G * ((1 + i) ** N - i * N - 1) / (i ** 2 * (1 + i) ** N)
 
 
 def linear_conversion(G=None, i=None, N=None):
-   fn = "gradient series (A/G, i, N): gives A"
-   if G is None:
-       raise FormulaError(fn, "G, or the first payment at period 2 is missing")
-   elif i is None:
-       raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-   elif N is None:
-       raise FormulaError(fn, "N, or number of number of interest periods is missing")
+    fn = "gradient series (A/G, i, N): gives A"
+    if G is None:
+        raise FormulaError(fn, "G, or the first payment at period 2 is missing")
+    elif i is None:
+        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
+    elif N is None:
+        raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-   return G * ( (1+i)**N - i*N - 1) / (i * ((1+i)**N - 1))
+    return G * ((1 + i) ** N - i * N - 1) / (i * ((1 + i) ** N - 1))
 
 
 def geo_present(A1=None, g=None, i=None, N=None):
-   fn = "geometric gradient series (P/A_1, g, i, N)"
-   if A1 is None:
-       raise FormulaError(fn, "A1, or the first payment at period 1 is missing")
-   elif g is None:
-       raise FormulaError(fn, "g, or the growth rate is missing")
-   elif i is None:
-       raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
-   elif N is None:
-       raise FormulaError(fn, "N, or number of number of interest periods is missing")
+    fn = "geometric gradient series (P/A_1, g, i, N)"
+    if A1 is None:
+        raise FormulaError(fn, "A1, or the first payment at period 1 is missing")
+    elif g is None:
+        raise FormulaError(fn, "g, or the growth rate is missing")
+    elif i is None:
+        raise FormulaError(fn, "i, or interest not provided: 0<=i<=1")
+    elif N is None:
+        raise FormulaError(fn, "N, or number of number of interest periods is missing")
 
-   if i == g:
-       return A1 * N / (1+i)
+    if i == g:
+        return A1 * N / (1 + i)
 
-   return A1 * (1 - (1+g)**N * (1+i)**(-N)) / (i-g)
+    return A1 * (1 - (1 + g) ** N * (1 + i) ** (-N)) / (i - g)
 
 
 def effective_rate_per_interest_period(r=None, M=None, K=None, C=None):
@@ -173,37 +180,82 @@ def effective_rate_per_interest_period(r=None, M=None, K=None, C=None):
     """
 
     if r is None:
-        raise FormulaError(fn, "r, or APR, or nominal interest rate per year is missing")
+        raise FormulaError(
+            fn, "r, or APR, or nominal interest rate per year is missing"
+        )
 
     if M == math.inf and K is not None:
-        return math.exp(r/K) -1
+        return math.exp(r / K) - 1
 
     if M == math.inf and K is None:
-        return math.exp(r/K) -1
+        return math.exp(r / K) - 1
 
     if C is not None and K is not None and M is None:
-        return (1+ r/(C*K))**C - 1
+        return (1 + r / (C * K)) ** C - 1
 
     if K is None and C is None and M is not None:
-        return (1 + r/M)**M - 1
+        return (1 + r / M) ** M - 1
 
     if C is not None and K is None and M is not None:
-        return (1+r/M)**C - 1
+        return (1 + r / M) ** C - 1
 
     if M is not None and K is not None and C is None and M is not None:
-        return (1+ r/M)**(M/K) -1
+        return (1 + r / M) ** (M / K) - 1
 
     raise FormulaError(fn, "invalid combo of inputs")
 
+
 # amortized
-def remaining_balance():
+def remaining_balance(A=None, i=None, N=None, n=None):
+    return eq_present(A, i, N=(N - n))
+
+
+def amortized_payment():
     pass
 
-def interest_payment():
+
+def interest_payment(A=None, i=None, N=None, n=None):
+    # return remaining_balance(A, i, N, n=(n - 1)) * i
+    return eq_present(A, i, N - n + 1) * i
+
+
+def total_interest(A=None, i=None, N=None):
+    sum = 0
+    for n in range(1, N + 1):
+        interest = interest_payment(A, i, N, n)
+        print(f"period {n}, interest: {interest}")
+        sum += interest
+    return sum
+
+
+def principal_repayment(PP, I):
     pass
 
-def principal_repayment():
-    pass
 
-def solve(expr, var: str):
-    pass
+def bond_payment(coupon, par, M):
+    return coupon * par / M
+
+
+def YTM(purchase_price, coupon, years, M, par):
+    # M=2 means semiannual
+    N = years * M  # number of payment periods
+
+    # division, but python doesn't do floats well...
+    A = sym.Rational(coupon * par, M)
+
+    i = sym.symbols("i")
+    print("A", A)
+    print("N", N)
+
+    expr = eq_present(A=A, i=i, N=N) + single_present(F=par, i=i, N=N) - purchase_price
+
+    return solve(expr, i)
+
+
+def bond_market(A, i, years, M, par):
+    N = years * M
+    return eq_present(A=A, i=i, N=N) + single_present(F=par, i=i, N=N)
+
+
+def solve(expr, var):
+    return sym.solveset(expr, var, domain=sym.S.Reals)
